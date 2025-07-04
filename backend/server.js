31ac6f7860e561e0260dee3eb27aeba888a5b712
frostyscupcake <joykrish.2003@gmail.com>
2025-07-04 13:25:58 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get("/:wildcard(*)", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });
 //
