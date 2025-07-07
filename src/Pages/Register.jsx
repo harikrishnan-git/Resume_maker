cda37cc461eee8e824b55ecb6f501b29a88ad888
harikrishnan-git <haribkrishh@gmail.com>
2025-07-07 21:48:28 +0530

    Defined toast at register

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
 export default function Register() {
   const [name, setName] = useState("");
